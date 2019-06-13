dc9d7a38ea859946a5e06116b83e985bc3902580
KaNiKir <kandzyuba@gmail.com>
2019-06-13 12:48:36 +0300

    Оптимизирован код

--- js/setup.js
+++ js/setup.js
@@ -10,31 +10,34 @@ var similarListElement = setup.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 // Подготовка Моков для тестирования
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-
-var surnames = ['да Марья', 'Верон', 'Мираблелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var MOCK = {
+  wizard: {
+    name: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+    surname: ['да Марья', 'Верон', 'Мираблелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+    coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+    eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
+  },
+};
 
 // Генерация массива объектов со случайными данными
-var wizards = [{}];
+var generateData = function () {
+  var wizards = [];
 
-var renderRandomInfo = function () {
   for (var i = 0; i < 4; i++) {
     wizards[i] = {
-      name: names[Math.floor(Math.random() * ((names.length - 1) - 0 + 1))] + ' ' + surnames[Math.floor(Math.random() * ((surnames.length - 1) - 0 + 1))],
-      coatColor: coatColors[Math.floor(Math.random() * ((coatColors.length - 1) - 0 + 1))],
-      eyesColor: eyesColors[Math.floor(Math.random() * ((eyesColors.length - 1) - 0 + 1))]
+      name: MOCK.wizard.name[Math.floor(Math.random() * ((MOCK.wizard.name.length - 1) - 0 + 1))] + ' ' + MOCK.wizard.surname[Math.floor(Math.random() * ((MOCK.wizard.surname.length - 1) - 0 + 1))],
+      coatColor: MOCK.wizard.coatColor[Math.floor(Math.random() * ((MOCK.wizard.coatColor.length - 1) - 0 + 1))],
+      eyesColor: MOCK.wizard.eyesColor[Math.floor(Math.random() * ((MOCK.wizard.eyesColor.length - 1) - 0 + 1))]
     };
   }
 
   return wizards;
 };
 
+var data = generateData();
+
 // Создание DOM элемента
-var renderWizard = function (wizard) {
+var createWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -48,9 +51,8 @@ var renderWizard = function (wizard) {
 var appendWizard = function () {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < wizards.length; i++) {
-    renderRandomInfo();
-    fragment.appendChild(renderWizard(wizards[i]));
+  for (var i = 0; i < data.length; i++) {
+    fragment.appendChild(createWizard(data[i]));
   }
   similarListElement.appendChild(fragment);
 };
